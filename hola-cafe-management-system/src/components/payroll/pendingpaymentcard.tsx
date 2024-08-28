2d6b505728c7d1fa325bd9854c1f1095bcba4fff
estrella <dommolino28@gmail.com>
2024-08-28 15:28:18 +0800

    add payroll card

---
+++ hola-cafe-management-system/src/components/payroll/pendingpaymentcard.tsx
@@ -0,0 +1,18 @@
+import { History } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
+
+const PendingPaymentCard = () => {
+  return (
+    <Card className="border border-gray-300 shadow-zinc-300">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Pending Payment</CardTitle>
+        <History className="p-2 bg-black text-white rounded-full" size={30} />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">₱15,000</div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PendingPaymentCard;
