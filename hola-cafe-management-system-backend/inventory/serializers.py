562504c5e0ec98e120d8ebf2e4732b971f86f67d
Adrian Gallano <adriangallanoengr@gmail.com>
2024-10-14 13:15:11 +0800

    Finish backend

--- hola-cafe-management-system-backend/inventory/serializers.py
+++ hola-cafe-management-system-backend/inventory/serializers.py
@@ -15,40 +15,11 @@ class SupplierSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_id = serializers.PrimaryKeyRelatedField(
-        queryset=Category.objects.all(), source="category"
-    )
-    supplier_id = serializers.PrimaryKeyRelatedField(
-        queryset=Supplier.objects.all(), source="supplier"
-    )
 
     class Meta:
         model = Product
         fields = "__all__"
 
-    def create(self, validated_data):
-        category = validated_data.pop("category")
-        supplier = validated_data.pop("supplier")
-
-        product = Product.objects.create(
-            category_id=category.id, supplier_id=supplier.id, **validated_data
-        )
-        return product
-
-    def update(self, instance, validated_data):
-        category = validated_data.pop("category", None)
-        supplier = validated_data.pop("supplier", None)
-
-        if category:
-            instance.category = category
-        if supplier:
-            instance.supplier = supplier
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-        return instance
 
 
 class ProductSupplierCategorySerializer(serializers.ModelSerializer):
