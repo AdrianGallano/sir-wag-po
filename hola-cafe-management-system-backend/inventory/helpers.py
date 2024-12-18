9eb4a0ebc980684127b0502513b940e3274184d4
Adrian Cris Gallano <adriangallanoengr@gmail.com>
2024-12-19 04:56:50 +0800

    edit is expired helper

--- hola-cafe-management-system-backend/inventory/helpers.py
+++ hola-cafe-management-system-backend/inventory/helpers.py
@@ -6,12 +6,16 @@ def check_expired():
     """
     Check if the stock is expired.
     """
-    stocks = (
-        Stock.objects.all()
-        .filter(is_expired=False)
-        .filter(expiration_date__lte=timezone.now())
-    )
+    stocks = Stock.objects.all()
+    
+    
+        # .filter(is_expired=False)
+        # .filter(expiration_date__lte=timezone.now())
+    
     
     for stock in stocks:
-        stock.is_expired = True
+        if stock.expiration_date <= timezone.now():
+            stock.is_expired = True
+        else:
+            stock.is_expired = False
         stock.save()
