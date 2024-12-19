cb4ae6649215c5b3ce015450d36e318fccd74a87
Adrian Cris Gallano <adriangallanoengr@gmail.com>
2024-12-19 13:26:12 +0800

    Fix expiration date bug

--- hola-cafe-management-system-backend/inventory/helpers.py
+++ hola-cafe-management-system-backend/inventory/helpers.py
@@ -6,16 +6,12 @@ def check_expired():
     """
     Check if the stock is expired.
     """
+    
     stocks = Stock.objects.all()
+    expired_stocks = stocks.filter(expiration_date__lte=timezone.now())
+    non_expired_stocks = stocks.filter(expiration_date__gt=timezone.now())
     
     
-        # .filter(is_expired=False)
-        # .filter(expiration_date__lte=timezone.now())
     
-    
-    for stock in stocks:
-        if stock.expiration_date <= timezone.now():
-            stock.is_expired = True
-        else:
-            stock.is_expired = False
-        stock.save()
+    expired_stocks.update(is_expired=True)
+    non_expired_stocks.update(is_expired=False)
